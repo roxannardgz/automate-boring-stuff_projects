c54c896099fa823dd8b168cd4829410176834194
Roxanna Rodriguez <106232287+roxannardgz@users.noreply.github.com>
2024-01-02 19:27:09 +0200

    add function addToInventory

--- Ch.5/fantasy_game_inventory.py
+++ Ch.5/fantasy_game_inventory.py
@@ -1,6 +1,4 @@
-## Fantasy Game Inventory
-import pprint
-
+### Fantasy Game Inventory
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 #print items in the inventory
@@ -12,6 +10,23 @@ def displayInventory(inventory):
         
         item_total += v #total of items
     print("Total number of items: " + str(item_total))
-    
-    
+
 displayInventory(stuff)
+
+
+
+
+### List to Dictionary for Fantasy Game Inventory
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
