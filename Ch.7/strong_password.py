06724bcda32278970a9204ba5e435a4cf9d853d0
Roxanna Rodriguez <106232287+roxannardgz@users.noreply.github.com>
2024-01-10 13:47:05 +0200

    Update strong_password.py
    - modified code for better readability and performance
    - fixed error messages typos
    - added special characters to the character count validation

--- Ch.7/strong_password.py
+++ Ch.7/strong_password.py
@@ -1,52 +1,32 @@
 ## Strong password detection
 import re
 
+#make sure password string is strong
 def isStrong(password):
+    
+    #password status
+    is_strong = True   #defaul value
+    
+    #password checks (strength criteria)
+    charater_count_rx = re.compile(r'[\w!@#$%&*()-=+^]{8,}')    #at least 8 characters long
+    lower_rx = re.compile(r'[a-z]')                             #includes lowercase char
+    upper_rx = re.compile(r'[A-Z]')                             #includes uppercase char
+    digit_rx = re.compile(r'\d')                                #includes digit(s)
+    
+    #dict with checks and error messages
+    checks = {1: (charater_count_rx, 'Error: The password must have at least 8 characters.'), 
+                2: (lower_rx, 'Error: The password must have at least one lowercase character.'), 
+                3: (upper_rx, 'Error: The password must have at least one uppercase character.'), 
+                4: (digit_rx, 'Error: The password must have at least one digit.')}
 
-    is_strong = True
-    
-    for i in range(1):
-        #has at least 8 characters long
-        password_rx = re.compile(r'\w{8,}')
-        
-        validation = password_rx.search(password)
-        
-        if not validation:
-            print('The password must have at least 8 digits.')
-            is_strong = False
-            break
+    #check password strength criteria
+    for check in checks.values():
+        if check[0].search(password) == None:
+            print(check[1])               #prints corresponding error message
+            is_strong = False             #changes password status
+            break                         #exit the loop - doesn't check for remaining validations
 
-        #contains both upper and lowercase characters
-        password_rx = re.compile(r'[a-z]') #lowecase
-        
-        validation = password_rx.search(password)
-        
-        if not validation:
-            print('The password must have at least one lowerchase character.')
-            is_strong = False
-            break
-        
-        password_rx = re.compile(r'[A-Z]') #uppercase
-        
-        validation = password_rx.search(password)
-        
-        if not validation:
-            print('The password must have at least one uppercase character.')
-            is_strong = False
-            break
-            
-            
-        #has at least one digit
-        password_rx = re.compile(r'\d')
-        
-        validation = password_rx.search(password)
-        
-        if not validation:
-            print('The password must have at least one digit.')
-            is_strong = False
-            break
-    
-    
+    #print message if all the checks are passed
     if is_strong == True:
         print('Success! Your password has been updated.')
 
