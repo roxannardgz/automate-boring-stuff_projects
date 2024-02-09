b04e89691f69921c0c2326049dc2b00c09b6fba5
Roxanna Rodriguez <106232287+roxannardgz@users.noreply.github.com>
2024-02-09 14:09:18 +0200

    Update multi-clipboard.py
    tidied up code
    fixed typo in print statement

--- Ch.9/multi-clipboard.py
+++ Ch.9/multi-clipboard.py
@@ -1,7 +1,7 @@
 #! python3
-#Updatable Multi-Clipboard - saves and loads pieces of text to the clipboard
+# Updatable Multi-Clipboard - saves and loads pieces of text to the clipboard
 
-#usage: save <keyword> - saves clipboard to keyword
+# usage: save <keyword> - saves clipboard to keyword
 #       delete <keyword> - deletes the keyword from the shelf
 #       <keyword> - loads keyword to clipboard
 #       list - loads all keywords to clipboard
@@ -9,28 +9,30 @@
 
 import shelve, pyperclip, sys
 
-mcbShelf = shelve.open('mcb')  #mcb stands for Multi-ClipBoard
+mcbShelf = shelve.open("mcb")  # mcb stands for Multi-ClipBoard
 
-## save clipboard content or delete keyword from shelf
+# save clipboard content or delete keyword from shelf
 if len(sys.argv) == 3:
-    #for saving
-   if sys.argv[1].lower() == 'save':
+    # for saving
+    if sys.argv[1].lower() == "save":
         mcbShelf[sys.argv[2]] = pyperclip.paste()
-    #for deleting
-    elif sys.argv[1].lower() == 'delete':
+    # for deleting
+    elif sys.argv[1].lower() == "delete":
         del mcbShelf[sys.argv[2]]
-## list keywords, load content or delete all keywords
+# list keywords, load content or delete all keywords
 elif len(sys.argv) == 2:
-    #for getting the list of keywords
-    if sys.argv[1].lower() == 'list':
+    # for getting the list of keywords
+    if sys.argv[1].lower() == "list":
         pyperclip.copy(str(list(mcbShelf.keys())))
-    #for deleting keywords
-    elif sys.argv[1].lower() == 'delete':
+    # for deleting keywords
+    elif sys.argv[1].lower() == "delete":
         mcbShelf.clear()
-    #for loading the content of the keyword
+    # for loading the content of the keyword
     elif sys.argv[1] in mcbShelf:
         pyperclip.copy(mcbShelf[sys.argv[1]])
 else:
-    print('Incorrect number of keywords. Please check the usage intructions for reference.')
+    print(
+        "Incorrect number of keywords. Please check the usage instructions for reference."
+    )
 
 mcbShelf.close()
